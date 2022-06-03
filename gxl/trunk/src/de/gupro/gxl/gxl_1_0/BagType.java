8d2148092b10c585a8cb7a3611abbfdf21103bf4
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2022-06-03 14:49:10 +0000

    Updated jaxb bindings to jakarta lib

--- gxl/trunk/src/de/gupro/gxl/gxl_1_0/BagType.java
+++ gxl/trunk/src/de/gupro/gxl/gxl_1_0/BagType.java
@@ -8,9 +8,9 @@
 
 package de.gupro.gxl.gxl_1_0;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
 
 
 /**
